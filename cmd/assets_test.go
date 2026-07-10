7fd100040567cb2359529ffa5b16101fa5eda5e5
cristianoliveira <contato@cristianoliveira.com.br>
2026-07-10 23:32:00 +0200

    test(assets): cover filtered export command

--- cmd/assets_test.go
+++ cmd/assets_test.go
@@ -1,13 +1,17 @@
 package cmd
 
 import (
+	"io"
 	"net/http"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/cristianoliveira/figma-cli/internal/cli"
 	"github.com/cristianoliveira/figma-cli/internal/extract"
 	"github.com/cristianoliveira/figma-cli/internal/figma"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAssetFilename(t *testing.T) {
@@ -26,6 +30,32 @@ func TestAssetNameFilenameSupportsExplicitPrefixTrimming(t *testing.T) {
 	asset := extract.Asset{ID: "1:2", Name: "icon/24/arrow-left / dark"}
 
 	assert.Equal(t, "arrow-left-dark", assetNameFilename(asset, "icon/24/"))
+}
+
+func TestAssetsCommandExportsFilteredAssets(t *testing.T) {
+	transport := roundTripFunc(func(request *http.Request) (*http.Response, error) {
+		body := "svg"
+		switch request.URL.Path {
+		case "/v1/files/FILE/nodes":
+			body = `{"nodes":{"1:2":{"document":{"id":"1:2","name":"Screen","type":"FRAME","children":[{"id":"2:3","name":"Close","type":"VECTOR"},{"id":"4:5","name":"Photo","fills":[{"type":"IMAGE"}]}]}}}}`
+		case "/v1/images/FILE":
+			assert.Equal(t, "2:3", request.URL.Query().Get("ids"))
+			body = `{"images":{"2:3":"https://cdn.example/close.svg"}}`
+		}
+		return &http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader(body)), Header: make(http.Header)}, nil
+	})
+	client := &figma.Client{HTTP: &http.Client{Transport: transport}}
+	outputDirectory := t.TempDir()
+
+	result := executeCommand(
+		newAssetsCommand(func() (*figma.Client, error) { return client, nil }, nil),
+		"https://www.figma.com/design/FILE/Screen?node-id=1-2", "--output", outputDirectory, "--kind", "icon", "--name", "close",
+	)
+
+	require.NoError(t, result.Err)
+	assert.Contains(t, result.Stdout, "close_2-3.svg")
+	assert.FileExists(t, filepath.Join(outputDirectory, "close_2-3.svg"))
+	assert.Contains(t, result.Stderr, "exported 1 asset(s), 0 failed")
 }
 
 func TestAssetsCommandRejectsFilenameModeBeforeLoadingClient(t *testing.T) {
